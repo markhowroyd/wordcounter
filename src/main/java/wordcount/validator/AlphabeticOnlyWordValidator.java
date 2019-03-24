455e404424fd1f29f0d7c745b98c1599fd3b4cc6
Mark Howroyd <mark.howroyd@gmail.com>
2019-03-24 12:38:45 +0000

    Validator update for foreign chars

--- src/main/java/wordcount/validator/AlphabeticOnlyWordValidator.java
+++ src/main/java/wordcount/validator/AlphabeticOnlyWordValidator.java
@@ -4,11 +4,11 @@ import java.util.regex.Pattern;
 
 public class AlphabeticOnlyWordValidator implements WordValidator {
 
-    private Pattern patternMatch = Pattern.compile("[a-zA-Z]+");
+    private Pattern patternMatch = Pattern.compile("\\w+");
 
     @Override
     public void assertValid(String word) throws InvalidWordException {
-        if (word == null || !patternMatch.matcher(word).matches()) {
+        if (word == null || word.isEmpty() || !word.chars().allMatch(Character::isLetter)) {
             throw new InvalidWordException("The word " + word + " is not valid - it must consist of alphabetic characters");
         }
     }
